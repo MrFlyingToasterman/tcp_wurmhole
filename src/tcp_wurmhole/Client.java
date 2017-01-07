0ce55afba733c7282db2ab73a43adfe45fca0dfb
Darius Musiolik <MrFlyingToasterman@yahoo.de>
2017-01-07 16:00:47 +0100

    server is now endlessloop

--- src/tcp_wurmhole/Client.java
+++ src/tcp_wurmhole/Client.java
@@ -15,34 +15,34 @@ import java.net.Socket;
  * @author Darius Musiolik
  */
 public class Client {
-    
+
     //This void starts the client
     public static void startclient() {
-        
+
         //I need to catch a possible exception
         try {
-            
-        //Creating socket for connection with localhost
-        Socket client = new Socket("127.0.0.1", 4444);
-        
-        //Streaming
-            
-        //Out Streams
-        OutputStream out = client.getOutputStream();
-        PrintWriter pwriter = new PrintWriter(out);
-        //In Streams
-        InputStream in = client.getInputStream();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-        
-        //Sending STuff to Server
-        pwriter.write("Say no more fam");
-        pwriter.flush();
-        
-        //Close reader and writer
-        reader.close();
-        pwriter.close();
-        
+
+            //Creating socket for connection with localhost
+            Socket client = new Socket("127.0.0.1", 4444);
+
+            //Streaming
+            //Out Streams
+            OutputStream out = client.getOutputStream();
+            PrintWriter pwriter = new PrintWriter(out);
+            //In Streams
+            InputStream in = client.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+            //Sending STuff to Server
+            pwriter.write("Say no more fam");
+            pwriter.flush();
+
+            //Close reader and writer
+            reader.close();
+            pwriter.close();
+
         } catch (Exception e) {
         }
+
     }
 }
--- src/tcp_wurmhole/Server.java
+++ src/tcp_wurmhole/Server.java
@@ -22,47 +22,50 @@ public class Server {
     //This void will run the Server
     public static void startserver() {
         
-        //I need to catch a possible exception
-        try {
-            //Creating Socket that will listen to the Port 4444
-            ServerSocket wurmhole = new ServerSocket(4444); 
-            whutil.log("Server starting", 1);
-            
-            //Creating incomming connection
-            Socket client = wurmhole.accept();
-            
-            //Streaming
-            
-            //Out Streams
-            OutputStream out = client.getOutputStream();
-            PrintWriter pwriter = new PrintWriter(out);
-            //In Streams
-            InputStream in = client.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-            
-            //Reading In streams
-            
-            String info_get = null;
-            String saved_information = "";
-            //Need a loop ? Why not Zoidberg...
-            for(;;) {
-                info_get = reader.readLine();
-                whutil.log("Getting from client =>" + info_get + "<", 1);
-                if (info_get == null) {
-                    break;
-                } else {
-                    saved_information = saved_information + info_get;
-                }
-            }
-            //Print complete information
-            whutil.log(saved_information, 1);
-            //Close reader and writer
-            reader.close();
-            pwriter.close();
-            
-        } catch (Exception e) {
-        }
+        do {
         
+            //I need to catch a possible exception
+            try {
+                //Creating Socket that will listen to the Port 4444
+                ServerSocket wurmhole = new ServerSocket(4444); 
+                whutil.log("Server starting", 1);
+
+                //Creating incomming connection
+                Socket client = wurmhole.accept();
+
+                //Streaming
+
+                //Out Streams
+                OutputStream out = client.getOutputStream();
+                PrintWriter pwriter = new PrintWriter(out);
+                //In Streams
+                InputStream in = client.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+                //Reading In streams
+
+                String info_get = null;
+                String saved_information = "";
+                //Need a loop ? Why not Zoidberg...
+                for(;;) {
+                    info_get = reader.readLine();
+                    whutil.log("Getting from client =>" + info_get + "<", 1);
+                    if (info_get == null) {
+                        break;
+                    } else {
+                        saved_information = saved_information + info_get;
+                    }
+                }
+                //Print complete information
+                whutil.log(saved_information, 1);
+                //Close reader and writer
+                reader.close();
+                pwriter.close();
+
+            } catch (Exception e) {
+            }
+            
+        }while(true);
     }
     
 }
