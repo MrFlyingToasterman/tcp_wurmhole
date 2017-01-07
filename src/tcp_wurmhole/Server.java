5a589dfcebd9121785c6bd1cbedbb0ca9ac61be3
dmusiolik <dmusiolik@localhost.localdomain>
2017-01-07 15:34:19 +0100

    added server streams

--- src/tcp_wurmhole/Server.java
+++ src/tcp_wurmhole/Server.java
@@ -3,6 +3,11 @@
  */
 package tcp_wurmhole;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -21,10 +26,39 @@ public class Server {
         try {
             //Creating Socket that will listen to the Port 4444
             ServerSocket wurmhole = new ServerSocket(4444); 
-            whutil.log("Server starting", 0);
+            whutil.log("Server starting", 1);
             
             //Creating incomming connection
             Socket client = wurmhole.accept();
+            
+            //Streaming
+            
+            //Out Streams
+            OutputStream out = client.getOutputStream();
+            PrintWriter pwriter = new PrintWriter(out);
+            //In Streams
+            InputStream in = client.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            
+            //Reading In streams
+            
+            String info_get = null;
+            String saved_information = "";
+            //Need a loop ? Why not Zoidberg...
+            for(;;) {
+                info_get = reader.readLine();
+                whutil.log("Getting from client =>" + info_get + "<", 1);
+                if (info_get == null) {
+                    break;
+                } else {
+                    saved_information = saved_information + info_get;
+                }
+            }
+            //Print complete information
+            whutil.log(saved_information, 1);
+            //Close reader and writer
+            reader.close();
+            pwriter.close();
             
         } catch (Exception e) {
         }
